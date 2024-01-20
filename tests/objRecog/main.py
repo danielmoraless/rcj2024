2b1093b0c3645c06914679e39b38dba4e3f439df
Daniel Morales <contactardanielmorales@gmail.com>
2024-01-20 15:46:21 -0500

    [Add] imageai detection tests

--- tests/objRecog/main.py
+++ tests/objRecog/main.py
@@ -1,10 +1,16 @@
 from imageai.Classification import ImageClassification
+from imageai.Detection import VideoObjectDetection
 import os
+import cv2
 
-def main():
-    cwd = os.getcwd()
+cwd = os.getcwd()
+assets = os.path.join(cwd, "assets\\")
+
+"""
+Clasificación de objectos con MobileNet V2
+"""
+def clasificacion():
     dotpth = os.path.join(cwd, "mobilenet_v2-b0353104.pth")
-    assets = os.path.join(cwd, "assets\\")
     img = os.path.join(assets, "img1.jpg")
 
     pred = ImageClassification()
@@ -14,7 +20,38 @@ def main():
 
     predic, prob = pred.classifyImage(img, result_count=2)
 
-    print(f"Predicciones: {predic}\nProbabilidades: {prob}")
+    print("\nResultados de clasificacion:")
+    print(f"\tPredicciones: {predic}\nProbabilidades: {prob}")
+
+"""
+Detección de objectos con RetinaNet
+"""
+
+# Analisis de cada frame
+def funcionPorFrame(n_frame, output, c_output):
+    print(f"\nInfo del frame #{n_frame}:")
+    print(f"\t- Info de cada objeto: {output}")
+    print(f"\t- Cuenta de objetos detectados: {c_output}")
+
+# Deteccion de objetos en vivo
+def deteccionEnVivo():
+    detec = VideoObjectDetection() # detector
+    detec.setModelTypeAsRetinaNet()
+    detec.setModelPath(os.path.join(cwd, "retinanet_resnet50_fpn_coco-eeacb38b.pth"))
+    detec.loadModel()
+    try:
+        camara = cv2.VideoCapture(0)
+        detec.detectObjectsFromVideo(camera_input=camara,
+                                    save_detected_video=False,
+                                    per_frame_function=funcionPorFrame,
+                                    frames_per_second=60)
+    finally:
+        camara.release()
+    print("\n[+] Deteccion en progreso...")
+
+def main():
+    clasificacion()
+    deteccionEnVivo()
 
 if __name__ == "__main__":
     main()
