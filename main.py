a0bba3cad76cc5efa43604fa934b4f81a519b05a
Daniel Morales <contactardanielmorales@gmail.com>
2024-01-26 12:49:05 -0500

    [Add] testing TCS3200

---
+++ main.py
@@ -0,0 +1,37 @@
+import RPi.GPIO as GPIO
+import lib.core.sensors.ColorSensor as ColorSensor
+
+GPIO.setmode(GPIO.BCM)
+
+cs1_p = {
+	"S2": 17,
+	"S3": 27,
+	"OUT": 22,
+}
+
+COLORES = {
+	"ROJO": [0, 0],
+	"AZUL": [0, 1],
+	"VERDE": [1, 1],
+	"SF": [1, 0],
+}
+
+def setup():
+	GPIO.setup(cs1_p["OUT"], GPIO.IN, pull_up_down=GPIO.PUD_UP)
+	GPIO.setup(cs1_p["S2"], GPIO.OUT)
+	GPIO.setup(cs1_p["S3"], GPIO.OUT)
+
+def loop():
+	sensor_de_color_1 = ColorSensor.TCS3200(GPIO, cs1_p)
+	while True:
+		rojo = sensor_de_color_1.read_once(COLORES["ROJO"], 10, 0.3)
+		azul = sensor_de_color_1.read_once(COLORES["AZUL"], 10, 0.3)
+		verde = sensor_de_color_1.read_once(COLORES["VERDE"], 10, 0.3)
+		print(f"R: {rojo}\nG: {verde}\nB: {azul}\n\n")
+
+if __name__ == "__main__":
+	setup()
+	try:
+		loop()
+	finally:
+		GPIO.cleanup()
