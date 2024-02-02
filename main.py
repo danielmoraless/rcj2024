2e928637a68333760f71c546e89501062478754f
Daniel Morales <contactardanielmorales@gmail.com>
2024-02-02 16:19:56 -0500

    [Add] primera prueba básica para motores

--- main.py
+++ main.py
@@ -1,24 +1,28 @@
 import RPi.GPIO as GPIO
-import lib.core.sensors.ColorSensor as ColorSensor
 import lib.core.utils.GeneralUtils as GeneralUtils
+import lib.core.actuators.Motors as Motors
 import conf
 
 GPIO.setmode(GPIO.BCM)
 
 def setup():
-	GPIO.setup(conf.colorSensor1["OUT"], GPIO.IN, pull_up_down=GPIO.PUD_UP)
 	GeneralUtils.setup_all(conf.pines)
 
+controlador = Motors.L298N(conf.l298n_p, 12500)
+controlador.start(0)
+
 def loop():
-	sensor_de_color_1 = ColorSensor.TCS3200(GPIO, conf.colorSensor1, 10, 0.1)
-	while True:
-		rgb = sensor_de_color_1.get_rgb()
-		color = sensor_de_color_1.color()
-		print(f"\n---\nRESULTADOS DE LECTURA:\n\t* RGB: {rgb}\n\t* COLOR:{color}\n---")
+	velocidad = int(input("Ingrese la velocidad: "))
+  controlador.forward(velocidad)
+  if input("¿Desea seguir? ") == "n":
+    controlador.stop()
+    GPIO.cleanup()
+    os.exit(0)
 
 if __name__ == "__main__":
-	setup()
 	try:
-		loop()
+    setup()
+		while True:
+      loop()
 	finally:
 		GPIO.cleanup()
