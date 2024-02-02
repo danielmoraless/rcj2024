42bb9cacc4be4597b3728f89f32026de653b7745
Daniel Morales <contactardanielmorales@gmail.com>
2024-02-02 16:47:09 -0500

    [Add] prueba mezclando sensor de colores y motor

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import RPi.GPIO as GPIO
 import lib.core.utils.GeneralUtils as GeneralUtils
 import lib.core.actuators.Motors as Motors
+import lib.core.sensors.ColorSensor as ColorSensor
 import conf
 
 GPIO.setmode(GPIO.BCM)
@@ -9,10 +10,11 @@ GeneralUtils.setup_all(conf.pines)
 
 controlador = Motors.L298N(conf.l298n_p, 12500)
 controlador.start(0)
+sensor_colores = ColorSensor.TCS3200(GPIO, conf.colorSensor1, 10, 0.01)
 
 def loop():
-	velocidad = float(input("Ingrese la velocidad: "))
-	controlador.forward(velocidad)
+	if sensor_colores.color() == "WHITE":
+		controlador.forward(90)
 
 if __name__ == "__main__":
 	try:
