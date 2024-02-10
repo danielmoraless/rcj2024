ca15468f3b037ed196ac4a2ec4e86b1da255c776
Daniel Morales <contactardanielmorales@gmail.com>
2024-02-10 09:26:35 -0500

    [Add] prueba rápida para los nuevos cambios en lib.core.actuators.Motors

--- main.py
+++ main.py
@@ -12,16 +12,26 @@ controlador = Motors.L298N(conf.l298n_p, 12500)
 controlador.start(0)
 sensor_colores = ColorSensor.TCS3200(GPIO, conf.colorSensor1, 10, 0.1)
 
+stopml = False
+
 def loop():
 	color = sensor_colores.color()
-	if color == "WHITE":
-		controlador.forward(90)
-	if color == "RED":
-		controlador.backward(90)
+	match color:
+		case "WHITE":
+			controlador.forward(95)
+		case "BLACK":
+			controlador.rotar_izquierda(95)
+		case "RED":
+			controlador.backward(95)
+		case "BLUE":
+			controlador.rotar_derecha(95)
+		case _:
+			print(f"color {color} no reconocido")
+			stopml = True
 
 if __name__ == "__main__":
 	try:
-		while True:
+		while not stopml:
 			loop()
 	except KeyboardInterrupt:
 		exit(0)
