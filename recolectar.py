7f767fc38757e7ed116c177ace4c370d165acfef
Daniel Morales <contactardanielmorales@gmail.com>
2024-02-23 18:44:13 -0500

    [Add] cleanup y toma de tiempo

--- recolectar.py
+++ recolectar.py
@@ -14,42 +14,51 @@ ciclos = int(input("[?] Número de ciclos de calibración: "))
 nlecturas = int(input("[?] Número de lecturas en cada ciclo: "))
 resultados = {}
 
-while True:
-	input("[...] Presione Enter para comenzar")
-	color = str(input("[?] Escriba el color a calibrar: "))
-	print(f"[+] Leyendo para {color}...")
+def calibrar():
+	while True:
+		input("[...] Presione Enter para comenzar")
+		color = str(input("[?] Escriba el color a calibrar: "))
+		print(f"[+] Leyendo para {color}...")
 
-	min_list = []
-	max_list = []
+		min_list = []
+		max_list = []
 
-	for i in range(ciclos):
-		lecturas = []
+		time_start = time.time()
+		for i in range(ciclos):
+			lecturas = []
 
-		for j in range(nlecturas):
-			lecturas.append(sum(sensor.get_rgb()))
+			for j in range(nlecturas):
+				lecturas.append(sum(sensor.get_rgb()))
 
-		lecturas = lecturas[1:] # generalmente, la primera lectura es incoherente. Omitimos
-		min_list.append(min(lecturas))
-		max_list.append(max(lecturas))
+			lecturas = lecturas[1:] # generalmente, la primera lectura es incoherente. Omitimos
+			min_list.append(min(lecturas))
+			max_list.append(max(lecturas))
 
-	minimo_promedio = sum(min_list)/ciclos
-	maximo_promedio = sum(max_list)/ciclos
-	resultados[color] = {
-		"min": minimo_promedio,
-		"max": maximo_promedio,
-	}
+		time_end = time.time()
+		minimo_promedio = sum(min_list)/ciclos
+		maximo_promedio = sum(max_list)/ciclos
+		resultados[color] = {
+			"min": minimo_promedio,
+			"max": maximo_promedio,
+		}
 
-	print(f"[+] {nlecturas*ciclos} lecturas realizadas para \"{color}\":")
-	print(f"\t[INFO] Mínimo: {minimo_promedio}")
-	print(f"\t[INFO] Máximo: {maximo_promedio}")
+		print(f"[+] {nlecturas*ciclos} lecturas realizadas en {time_end-time_start}s para \"{color}\":")
+		print(f"\t[INFO] Mínimo: {minimo_promedio}")
+		print(f"\t[INFO] Máximo: {maximo_promedio}")
 
-	if input("[?] ¿Desea continuar? (y/n): ") == "n":
-		break
+		if input("[?] ¿Desea continuar? (y/n): ") == "n":
+			break
 
-data_dir = "florence_data"
-os.makedirs(data_dir, exist_ok=True)
+	data_dir = "florence_data"
+	os.makedirs(data_dir, exist_ok=True)
 
-with open(os.path.join(data_dir, "calibrate.json"), "w") as data_file:
-	data_file.write(json.dumps(resultados))
+	with open(os.path.join(data_dir, "calibrate.json"), "w") as data_file:
+		data_file.write(json.dumps(resultados))
 
-print(f"[+] ¡Datos guardados en {os.getcwd()}/{data_dir}/calibrate.json!")
+	print(f"[+] ¡Datos guardados en {os.getcwd()}/{data_dir}/calibrate.json!")
+
+if __name__ == "__main__":
+	try:
+		calibrar()
+	finally:
+		GPIO.cleanup()
