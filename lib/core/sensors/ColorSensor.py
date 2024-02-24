fea528c074fdb9b3a761318860039afe5d601901
Daniel Morales <contactardanielmorales@gmail.com>
2024-02-24 15:55:28 -0500

    fix: color name is None initialy

--- lib/core/sensors/ColorSensor.py
+++ lib/core/sensors/ColorSensor.py
@@ -56,9 +56,9 @@ class TCS3200:
 		rgb = self.get_rgb()
 		suma = sum(rgb)
 
-		for colorName in self.ref:
-			if self.debug:
-				print(f"DEBUG (TCS3200.color): {(colorName, suma, rgb)}")
+		if self.debug:
+			print(f"DEBUG (TCS3200.color): {(suma, rgb)}")
 
+		for colorName in self.ref:
 			if suma >= self.ref[colorName]["min"] and suma <= self.ref[colorName]["max"]:
 				return colorName
