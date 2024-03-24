ff9ffdae0c61af6757ff8abd8b23a4347fbd6079
Daniel Morales <contactardanielmorales@gmail.com>
2024-03-24 14:15:31 -0500

    cambios para pruebas de seguidor de línea

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import RPi.GPIO as GPIO
 import lib.core.utils.GeneralUtils as GeneralUtils
+from lib.core.sensors.ColorSensor import TCS3200
+from lib.core.sensors.IRSensor import IR
+from lib.core.actuators.Motors import L298N
+from lib.LineFollower import Follower
 import conf
 
 trigger = False
@@ -17,9 +21,20 @@ def setup():
 	GeneralUtils.setup_all(conf.pines)
 	GPIO.setup(conf.BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
+	global sensor_color1
+	global sensor_color2
+	global ir
+	global motores
+	global seguidor
+
+	sensor_color1 = TCS3200(conf.colorSensor1)
+	sensor_color2 = TCS3200(conf.colorSensor2)
+	ir = IR(conf.IR_1, conf.IR_2)
+	motores = L298N(conf.l298n_p, 15000)
+	seguidor = Follower(sensor_color1, sensor_color2, motores, ir)
+
 def loop():
-	# codigo principal
-	pass
+	seguidor.calculate_direction_by_color()
 
 if __name__ == "__main__":
 	try:
